cd49f0b8f8783c67109dd42b47b33a640c003704
Smiler51 <96702317+smileman51@users.noreply.github.com>
2022-09-21 16:31:12 -0700

    Update schema.js

--- schema.js
+++ schema.js
@@ -4,7 +4,7 @@ script.textContent = JSON.stringify({
     "@context": "https://schema.org",
     "@type": "Organization",
     "name": 'Co-Browsing',
-    "address": 'проспект Валерія Лобановського, 6А, Київ, 03037',
+    "address": 'Ð¿Ñ€Ð¾ÑÐ¿ÐµÐºÑ‚ Ð’Ð°Ð»ÐµÑ€Ñ–Ñ Ð›Ð¾Ð±Ð°Ð½Ð¾Ð²ÑÑŒÐºÐ¾Ð³Ð¾, 6Ð, ÐšÐ¸Ñ—Ð², 03037',
     "telephone": '+380665737232',
     "description": 'Co-Browsing is the tool to share web browser screen online. With our standalone co-browsing solution, you can assist your friends or surf the web together. Cobrowsing is ideal for any sales and support team. Help your clients in real time.',
     "url": 'https://co-browsing.net/',
